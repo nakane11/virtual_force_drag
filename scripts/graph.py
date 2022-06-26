1dffa74a32742919ad0253ec22c9465309030831
Aoi Nakane <nakane@jsk.imi.i.u-tokyo.ac.jp>
2022-06-26 12:25:43 +0900

    Update

--- scripts/graph.py
+++ scripts/graph.py
@@ -3,27 +3,40 @@ import pandas as pd
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 
-path = input('csv file path:')
-df = pd.read_csv(path)
-df.set_index('time', inplace = True)
+path_vel = input('csv velocity file path:')
+df_vel = pd.read_csv(path_vel)
+path_force = input('csv force file path:')
+df_force = pd.read_csv(path_force)
+df_vel.set_index('time', inplace = True)
+df_force.set_index('time', inplace = True)
 
-plt.figure()
+fig = plt.figure(figsize=(8, 5))
 plt.rcParams['font.family'] = "DejaVu Serif"
 plt.rcParams['mathtext.fontset'] = 'stix'
 plt.rcParams["font.size"] = 18
 
-axes = df[['force_x', 'vel_x']].plot.line(subplots=True, layout=(2, 1), style=['r','r'], figsize=(10, 8))
-df[['force_y']].plot.line(ax=axes[0][0], style=['g'])
-df[['vel_y']].plot.line(ax=axes[1][0], style=['g'])
+ax1 = fig.add_subplot(2, 1, 1)
+df_force.plot.line(ax=ax1)
+ax2 = fig.add_subplot(2, 1, 2)
+df_vel.plot.line(ax=ax2)
 
-axes[0][0].set_ylabel('force')
-axes[1][0].set_ylabel("velocity")
-axes[1][0].set_xlabel('time / s')
-axes[0][0].set_yticks([])
-axes[1][0].set_yticks([])
+ax1.set_xlim(0, 12)
+ax2.set_xlim(0, 12)
+ax1.set_ylabel('force')
+ax2.set_ylabel('velocity')
+ax2.set_xlabel('time / s')
 
-axes[0][0].legend([r'$F_x$', r'$F_y$'], loc='upper right')
-axes[1][0].legend([r'$v_x$', r'$v_y$'], loc='upper right')
+ax1.set_xticks([0,2,4,6,8,10,12])
+ax2.set_xticks([0,2,4,6,8,10,12])
+ax1.label_outer()
+ax2.label_outer()
+
+ax1.legend([r'$F_x$', r'$F_y$'], loc='upper right')
+ax2.legend([r'$v_x$', r'$v_y$'], loc='upper right')
+
+ax1.yaxis.set_label_coords(-0.1, 0.5)
+ax2.yaxis.set_label_coords(-0.1, 0.5)
+fig.tight_layout()
 
 plt.savefig('output.png')
 plt.close('all')
