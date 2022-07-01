a7e8d99075552b6a6d5c1d5c0dd976092905bccf
Aoi Nakane <nakane@jsk.imi.i.u-tokyo.ac.jp>
2022-07-01 12:52:53 +0900

    Add pull-arm-moving-average.l

--- scripts/graph.py
+++ scripts/graph.py
@@ -5,20 +5,25 @@ import matplotlib.pyplot as plt
 
 path_vel = input('csv velocity file path:')
 df_vel = pd.read_csv(path_vel)
-path_force = input('csv force file path:')
-df_force = pd.read_csv(path_force)
-df_vel.set_index('time', inplace = True)
-df_force.set_index('time', inplace = True)
+path_force_smooth = input('csv smooth force file path:')
+df_force_smooth = pd.read_csv(path_force_smooth)
+path_force_transform = input('csv transform force file path:')
+df_force_transform = pd.read_csv(path_force_transform)
 
-fig = plt.figure(figsize=(8, 5))
+df_vel.set_index('time', inplace = True)
+df_force_smooth.set_index('time', inplace = True)
+df_force_transform.set_index('time', inplace = True)
+
+fig = plt.figure(figsize=(15, 10))
 plt.rcParams['font.family'] = "DejaVu Serif"
 plt.rcParams['mathtext.fontset'] = 'stix'
-plt.rcParams["font.size"] = 18
+plt.rcParams["font.size"] = 22
 
 ax1 = fig.add_subplot(2, 1, 1)
-df_force.plot.line(ax=ax1)
+df_force_smooth.plot.line(ax=ax1, style=['r', 'g'])
+df_force_transform.plot.line(ax=ax1, style=['r:', 'g:'])
 ax2 = fig.add_subplot(2, 1, 2)
-df_vel.plot.line(ax=ax2)
+df_vel.plot.line(ax=ax2, style=['r', 'g'])
 
 ax1.set_xlim(0, 12)
 ax2.set_xlim(0, 12)
@@ -31,11 +36,13 @@ ax2.set_xticks([0,2,4,6,8,10,12])
 ax1.label_outer()
 ax2.label_outer()
 
-ax1.legend([r'$F_x$', r'$F_y$'], loc='upper right')
-ax2.legend([r'$v_x$', r'$v_y$'], loc='upper right')
+# ax1.legend([r'$F_x$', r'$F_y$'], loc='upper left', bbox_to_anchor=(1, 1))
+# ax2.legend([r'$v_x$', r'$v_y$'], loc='upper left', bbox_to_anchor=(1, 1))
+ax1.legend([r'$F_x$', r'$F_y$'], loc='lower right')
+ax2.legend([r'$v_x$', r'$v_y$'], loc='lower right')
 
-ax1.yaxis.set_label_coords(-0.1, 0.5)
-ax2.yaxis.set_label_coords(-0.1, 0.5)
+ax1.yaxis.set_label_coords(-0.07, 0.5)
+ax2.yaxis.set_label_coords(-0.07, 0.5)
 fig.tight_layout()
 
 plt.savefig('output.png')
