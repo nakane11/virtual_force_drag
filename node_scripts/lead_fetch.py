ab604a58882356acec136f0ba3e8f66c957c2049
Aoi Nakane <nakane@jsk.imi.i.u-tokyo.ac.jp>
2022-09-26 19:10:43 +0900

    fix vx=0

--- node_scripts/lead_fetch.py
+++ node_scripts/lead_fetch.py
@@ -13,9 +13,8 @@ class LeadFetch(object):
         self.valid_duration = rospy.get_param('~valid_duration', 1)
         self.timer_running = False
         self.fx_threshold = 0.6
-        self.fy_threshold = 0.7
+        self.fy_threshold = 0.8
         self.vx_max = 0.7
-        self.vy_max = math.pi * 0.3
         self.reset()
         self.pub = rospy.Publisher('~output', Twist, queue_size=1)
         self.status_pub = rospy.Publisher('~status', Bool, queue_size=1, latch=True)
@@ -31,17 +30,14 @@ class LeadFetch(object):
 
     def reset(self):
         self.status_x = 'STOP'
-        self.status_y = 'STOP'
         self.fx = 0
-        self.fy = 0
         self.vx = 0
         self.vy = 0
         self.sign_x = 1
-        self.sign_y = 1
 
     def wrench_cb(self, msg):
         self.fx = (msg.wrench.force.x - 1.5)/7
-        # self.fy = (msg.wrench.force.y - 0.8)/3.5
+        self.fy = (msg.wrench.force.y - 0.8)/3.5
         self.last_updated_time = rospy.Time.now()
 
     def execute_x(self, f):
@@ -80,51 +76,20 @@ class LeadFetch(object):
         elif self.status_x == 'DECEL':
             self.vx -= self.sign_x * self.deceleration_x(f)
             rospy.loginfo('DECEL: {}'.format(self.deceleration_x(f)))
-            if abs(self.vx) < 0.03:
+            if self.vx * self.sign_x < 0:
                 self.vx = 0
                 self.status_x == 'STOP'
         self.sign_x = 1 if self.vx > 0 else -1
         if abs(self.vx) > self.vx_max:
             self.vx = self.sign_x * self.vx_max
 
-    # def execute_y(self, f):
-    #     if self.status_y == 'STOP':
-    #         rospy.loginfo('STOP')
-    #         if abs(f) > self.fy_threshold:
-    #             self.status_y = 'ACCEL'
-    #         return
-                
-    #     elif self.status_y == 'STEADY':
-    #         rospy.loginfo('STEADY')
-    #         if abs(f) > self.fy_threshold:
-    #             if f * self.vy < 0:
-    #                 self.status_y = 'DECEL'
-    #             elif f * self.vy > 0:
-    #                 self.status_y = 'ACCEL'
+    def execute_y(self, f):
+        if abs(f) > self.fy_threshold:
+            self.vy = 0.1 * f
+            rospy.loginfo('rotate')
+        else:
+            self.vy = 0
             
-    #     elif self.status_y == 'ACCEL':
-    #         if abs(f) > self.fy_threshold:
-    #             if f * self.vy < 0:
-    #                 self.status_y = 'DECEL'
-    #         elif abs(f) < self.fy_threshold:
-    #             self.status_y = 'STEADY'
-            
-    #     elif self.status_y == 'DECEL':
-    #         if abs(f) > self.fy_threshold:
-    #             if f * self.vy < 0:
-    #                 self.status_y = 'DECEL'
-    #         elif abs(f) < self.fy_threshold:
-    #             self.status_y = 'STEADY'
-
-    #     if self.status_y == 'ACCEL':
-    #         self.vy += self.acceleration_y(f)
-    #         rospy.loginfo('ACCEL: +{}'.format(self.acceleration_y(f)))
-    #         if abs(self.vy) > self.vy_max:
-    #             self.vy = (1 if f>0 else -1)*self.vy_max
-    #     elif self.status_y == 'DECEL':
-    #         self.vy += self.deceleration_y(f)
-    #         rospy.loginfo('DECEL: {}'.format(self.deceleration_y(f)))
-                
     def timer_cb(self, timer):
         fx = self.fx
         fy = self.fy
@@ -133,7 +98,7 @@ class LeadFetch(object):
             return
 
         self.execute_x(fx)
-        # self.execute_y(fy)
+        self.execute_y(fy)
         self.send_cmd_vel(x=self.vx, d=self.vy)
         
     def send_cmd_vel(self, x=0, d=0):
@@ -148,12 +113,6 @@ class LeadFetch(object):
 
     def deceleration_x(self, f):
         return 0.002 * math.exp(3*abs(f))
-
-    # def acceleration_y(self, f):
-    #     return (1 if f>0 else -1)*0.003 * math.log(1.5*abs(f))
-
-    # def deceleration_y(self, f):
-    #     return (1 if f>0 else -1)*0.1 * (1-math.exp((-abs(f))/5))
 
     def start_timer(self, req):
         if not self.timer_running:
